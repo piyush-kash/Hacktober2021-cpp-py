af6b1c792e19d66c41c707fd69a9e759e6fa5dcb
Prathviraj Prabhu <69745202+itsprathvi@users.noreply.github.com>
2021-10-21 22:54:49 +0530

    Added Balanced Brackets programme file
    This program which determine whether the given brackets are balanced or not

---
+++ balanced_brackets.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+bool isbalance(string s){
+    stack<char> st;
+    for (int it = 0;it<s.size();it++)
+    {
+        if(s[it] == '(' || s[it] == '{' || s[it] == '[')
+            st.push(s[it]);
+        else
+            if(st.empty())return false;
+            char x = st.top();
+            st.pop();
+            if (x == '(' && s[it] == ')' || x == '[' && s[it] == ']' || x == '{' && s[it] == '}')continue;
+            else
+                return false;
+    }
+    return st.empty();
+}
