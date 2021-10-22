7f7ba41ff609811b182cb689181690c339d37035
Ujwal Kandi <51212164+UjwalKandi@users.noreply.github.com>
2021-10-22 19:33:33 +0530

    Update Roulette-Game.py

--- Roulette-Game.py
+++ Roulette-Game.py
@@ -1,4 +1,10 @@
-
+"""
+Roulette-Game:
+Python program simulates the game of roulette. In roulette, a wheel spins 
+and yields a number between 1 and 49 when a ball drops into a numbered slot. 
+The initial amount is monitored throughout the game and will be reminded to 
+the player after each guess. If the amount goes zero, then the game ends.
+"""
 from random import randint
 
 totalAmount=1000                            #total amount invested 
